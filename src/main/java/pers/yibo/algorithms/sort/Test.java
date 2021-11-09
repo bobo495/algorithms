2e76200c967b9cdeb0f7130d3e2d52b108eb59b2
perayb <aybthebest@yahoo.com>
2021-11-09 18:09:34 +0800

    排序功能测试

---
+++ src/main/java/pers/yibo/algorithms/sort/Test.java
@@ -0,0 +1,16 @@
+package pers.yibo.algorithms.sort;
+
+import java.util.Arrays;
+
+/**
+ * @author yibo
+ * @date 2021-11-09 17:59
+ **/
+public class Test {
+    public static void main(String[] args) {
+        Integer[] a = new Integer[]{1, 5, 7, 3, 9, 4, 8, 5, 6, 2};
+        Selection.sort(a);
+        System.out.println(Utils.isSorted(a));
+        System.out.println(Arrays.toString(a));
+    }
+}
