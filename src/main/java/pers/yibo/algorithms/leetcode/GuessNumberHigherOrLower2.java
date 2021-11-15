897c614b94622d90873b225248dc7998e021526b
perayb <aybthebest@yahoo.com>
2021-11-15 17:31:08 +0800

    375. 猜数字大小 II
    DP问题

--- src/main/java/pers/yibo/algorithms/leetcode/GuessNumberHigherOrLower2.java
+++ src/main/java/pers/yibo/algorithms/leetcode/GuessNumberHigherOrLower2.java
@@ -18,67 +18,24 @@ package pers.yibo.algorithms.leetcode;
  * @date 2021-11-12 10:09
  **/
 public class GuessNumberHigherOrLower2 {
+    /**
+     * 参考：https://leetcode-cn.com/problems/guess-number-higher-or-lower-ii/solution/cai-shu-zi-da-xiao-ii-by-leetcode-soluti-a7vg/
+     */
     public int getMoneyAmount(int n) {
-        if (n <= 5) {
-            int[] dp = new int[5];
-            dp[0] = 0;
-            dp[1] = 1;
-            dp[2] = 2;
-            dp[3] = 5;
-            dp[4] = 7;
-            return dp[n - 1];
-        }
-
-        int[] dp = new int[n];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 5;
-        dp[4] = 7;
-
-        // 根节点逐个遍历，左侧子树最小值为dp[head - 2],右侧子树最小值为完全二叉树的最大父节点值之和
-        for (int i = 5; i < n; i++) {
-            int d = 4;
-            dp[i] = Integer.MAX_VALUE;
-            int searchIndex = i + 2 - d;
-            // 每次right构建完全二叉树，找到此时符合条件的minIndex
-            int cost;
-            int minIndex = searchIndex;
-            while (searchIndex > 2) {
-                cost = searchIndex + Math.max(dp[searchIndex - 2], rightMin(searchIndex + 1, i + 1));
-                if (cost < dp[i]) {
-                    dp[i] = cost;
-                    minIndex = searchIndex;
+        int[][] dp = new int[n + 1][n + 1];
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = i + 1; j <= n; j++) {
+                dp[i][j] = j + dp[i][j - 1];
+                for (int k = i; k < j; k++) {
+                    dp[i][j] = Math.min(dp[i][j], k + Math.max(dp[i][k - 1], dp[k + 1][j]));
                 }
-                d *= 2;
-                searchIndex = i + 2 - d;
             }
-            do {
-                minIndex++;
-                cost = minIndex + Math.max(dp[minIndex - 2], rightMin(minIndex + 1, i + 1));
-                if (cost < dp[i]) {
-                    dp[i] = cost;
-                }
-            } while (cost <= dp[i]);
         }
-
-        return dp[n - 1];
-    }
-
-    public int rightMin(int start, int end) {
-        int min = 0;
-        while (end - start > 1) {
-            start = (start + end) / 2 + 1;
-            min += start - 1;
-        }
-        return min;
+        return dp[1][n];
     }
 
     public static void main(String[] args) {
         GuessNumberHigherOrLower2 g = new GuessNumberHigherOrLower2();
-        System.out.println(g.getMoneyAmount(130));
-        for (int i = 1; i <= 4; i++) {
-            System.out.println(i + " " + g.rightMin(i, 7));
-        }
+        System.out.println(g.getMoneyAmount(5));
     }
 }
