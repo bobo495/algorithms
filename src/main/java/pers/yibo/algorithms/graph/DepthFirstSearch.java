e5069c42e97301c279361efb86d6642e8529edc4
perayb <aybthebest@yahoo.com>
2021-11-18 14:23:38 +0800

    DFS 遍历图

---
+++ src/main/java/pers/yibo/algorithms/graph/DepthFirstSearch.java
@@ -0,0 +1,57 @@
+package pers.yibo.algorithms.graph;
+
+/**
+ * 深度优先搜索
+ *
+ * 深度优先搜索标记与起点连通的所有顶点所需的时间和顶点的度数之和成正比。
+ *
+ * @author yibo
+ * @date 2021-11-18 14:11
+ **/
+public class DepthFirstSearch {
+    /**
+     * 顶点是否被搜索过
+     */
+    private final boolean[] marked;
+    /**
+     * 被搜索过的顶点数量
+     */
+    private int count;
+
+    /**
+     * DFS方法搜索图
+     *
+     * @param graph 指定图
+     * @param s     搜索起点
+     */
+    public DepthFirstSearch(Graph graph, int s) {
+        marked = new boolean[graph.getVertices()];
+    }
+
+    /**
+     * 递归地访问它的所有没有被标记过的邻居顶点
+     *
+     * @param graph 指定图
+     * @param v     当前搜索点
+     */
+    public void dfs(Graph graph, int v) {
+        // 新搜索的点标记为true，并更新count
+        marked[v] = true;
+        count++;
+        // 检索该点的邻接点集
+        for (int w : graph.getAdjacencyVertices(v)) {
+            if (!marked[v]) {
+                // 找到未被标记的邻接点，继续往下搜索
+                dfs(graph, w);
+            }
+        }
+    }
+
+    public boolean marked(Graph graph, int v) {
+        return marked[v];
+    }
+
+    public int count() {
+        return count;
+    }
+}
