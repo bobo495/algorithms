ae1d120776303dddca3d4b68bf0ddbbbce8c4c55
perayb <aybthebest@yahoo.com>
2021-11-18 16:52:11 +0800

    add Topological DAG的拓扑排序

---
+++ src/main/java/pers/yibo/algorithms/graph/Topological.java
@@ -0,0 +1,30 @@
+package pers.yibo.algorithms.graph;
+
+/**
+ * 有向无环图的拓扑排序
+ * <p>
+ * {@link DepthFirstOrder#reversePost()}的逆后序排序
+ *
+ * @author yibo
+ * @date 2021-11-18 16:47
+ **/
+public class Topological {
+    /**
+     * 顶点的拓扑顺序
+     */
+    private final Iterable<Integer> order;
+
+    public Topological(Graph graph) {
+        DirectedCycle cycle = new DirectedCycle(graph);
+        if (cycle.hasCycle()) {
+            throw new IllegalArgumentException("Graph is not DAG, cycle is: " + cycle.getCycle());
+        }
+
+        DepthFirstOrder dfo = new DepthFirstOrder(graph);
+        order = dfo.reversePost();
+    }
+
+    public Iterable<Integer> order() {
+        return order;
+    }
+}
