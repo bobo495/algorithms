7f0a2c99bd5c9686e7ee47f067918306dbbc115f
perayb <aybthebest@yahoo.com>
2021-11-25 09:56:41 +0800

    基于新构建的接口改写最小优先队列

--- src/main/java/pers/yibo/algorithms/sort/MinIndexPriorityQueue.java
+++ src/main/java/pers/yibo/algorithms/sort/MinIndexPriorityQueue.java
@@ -1,8 +1,5 @@
 package pers.yibo.algorithms.sort;
 
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-
 /**
  * 索引优先队列 - 小顶堆
  *
@@ -25,38 +22,12 @@ public class MinIndexPriorityQueue<T extends Comparable<? super T>> extends Abst
     }
 
     @Override
-    public Iterator<Integer> iterator() {
-        return new HeapIterator();
-    }
-
-    private class HeapIterator implements Iterator<Integer> {
-
-        private final MinIndexPriorityQueue<T> copy;
-
-        public HeapIterator() {
-            copy = new MinIndexPriorityQueue<>(priorityQueue.length - 1);
-            for (int i = 1; i <= size; i++) {
-                copy.put(priorityQueue[i], items[priorityQueue[i]]);
-            }
+    MinIndexPriorityQueue<T> copy() {
+        MinIndexPriorityQueue<T> copy = new MinIndexPriorityQueue<>(this.priorityQueue.length - 1);
+        for (int i = 1; i <= size; i++) {
+            copy.put(this.priorityQueue[i], this.items[priorityQueue[i]]);
         }
-
-        @Override
-        public boolean hasNext() {
-            return !copy.isEmpty();
-        }
-
-        @Override
-        public void remove() {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public Integer next() {
-            if (!hasNext()) {
-                throw new NoSuchElementException();
-            }
-            return copy.pollIndex();
-        }
+        return copy;
     }
 
     public static void main(String[] args) {
@@ -65,19 +36,20 @@ public class MinIndexPriorityQueue<T extends Comparable<? super T>> extends Abst
         queue.put(1, "a");
         queue.put(5, "e");
         queue.put(4, "d");
-        queue.iterator().forEachRemaining(System.out::println);
+        queue.getIndices().forEachRemaining(System.out::println);
+        queue.getItems().forEachRemaining(System.out::println);
         System.out.println("======END======");
 
         queue.put(3, "c");
-        queue.iterator().forEachRemaining(System.out::println);
+        queue.getItems().forEachRemaining(System.out::println);
         System.out.println("======END======");
 
         queue.put(3, "g");
-        queue.iterator().forEachRemaining(System.out::println);
+        queue.getItems().forEachRemaining(System.out::println);
         System.out.println("======END======");
 
         queue.delete(4);
-        queue.iterator().forEachRemaining(System.out::println);
+        queue.getItems().forEachRemaining(System.out::println);
         System.out.println("======END======");
     }
 }
