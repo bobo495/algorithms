a1adc04873f3ce3a391de548359643a50a720e55
perayb <aybthebest@yahoo.com>
2021-12-06 09:13:46 +0800

    1816. 截断句子

---
+++ src/main/java/pers/yibo/algorithms/leetcode/TruncateSentence.java
@@ -0,0 +1,28 @@
+package pers.yibo.algorithms.leetcode;
+
+/**
+ * 1816. 截断句子
+ * <p>
+ * https://leetcode-cn.com/problems/truncate-sentence
+ * <p>
+ * 句子 是一个单词列表，列表中的单词之间用单个空格隔开，且不存在前导或尾随空格。每个单词仅由大小写英文字母组成（不含标点符号）。
+ * <p>
+ * 例如，"Hello World"、"HELLO" 和 "hello world hello world" 都是句子。
+ * 给你一个句子 s 和一个整数 k ，请你将 s 截断 ,使截断后的句子仅含 前 k 个单词。返回 截断 s 后得到的句子。
+ *
+ * @author yibo
+ * @date 2021-12-06 09:10
+ **/
+public class TruncateSentence {
+    public String truncateSentence(String s, int k) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                k--;
+                if (k == 0) {
+                    return s.substring(0, i);
+                }
+            }
+        }
+        return s;
+    }
+}
